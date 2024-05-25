8bbd2759cc2a81ba30c12ac190530041b7e98323
ReyRiordan <reyriordan@gmail.com>
2024-05-24 20:38:25 -0400

    better UI layout

--- webapp.py
+++ webapp.py
@@ -10,34 +10,43 @@ st.title("Generate Experimental Stimuli")
 
 client = OpenAI()
 
-st.header("Word")
 st.session_state['word'] = st.text_input("Word:")
 
 
-st.header("Audio")
+st.divider()
 
-if st.button("Generate Audio") and st.session_state['word']:
+if st.columns(3)[1].button("Generate Audio", type="primary", use_container_width=True) and st.session_state['word']:
     st.session_state['audio'] = audio.generate_openai(st.session_state['word'], client)
     
 if 'audio' in st.session_state:
-    st.button(label="Play",
+    audio_layout = st.columns(4)
+
+    audio_layout[1].button(label="Play",
               on_click=audio.play,
-              args=[st.session_state['audio']])
-    st.download_button(label="Download",
+              args=[st.session_state['audio']],
+              use_container_width=True)
+    
+    audio_layout[2].download_button(label="Download",
                        data=st.session_state['audio'],
                        file_name=st.session_state['word']+".mp3",
-                       mime="audio/mpeg")
+                       mime="audio/mpeg",
+                       use_container_width=True)
 
 
-st.header("Image")
+st.divider()
 
-if st.button("Generate Image") and st.session_state['word']:
+if st.columns(3)[1].button("Generate Image", type="primary", use_container_width=True) and st.session_state['word']:
     st.session_state['image'] = image.edit_openai(st.session_state['word'], client)
 
 if 'image' in st.session_state:
-    st.image(image=st.session_state['image'],
+    image_layout = st.columns([3, 1, 1, 1])
+
+    image_layout[0].image(image=st.session_state['image'],
              output_format="PNG")
-    st.download_button(label="Download",
+    
+    for i in range(4):
+        image_layout[2].markdown("#")
+    image_layout[2].download_button(label="Download",
                        data=st.session_state['image'],
                        file_name=st.session_state['word']+".png",
                        mime="image/png")
